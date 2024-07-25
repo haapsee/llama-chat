6748e67bb67fdd241b8a413cd821d56fded0f807
Eemil Haapsaari <eemil@haapsaari.fi>
2024-07-25 19:45:24 +0300

    Working chat with llama 2

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
+import ctransformers
 import transformers
 import torch
 
-some_tensor = torch.randn(1000, 1000).cuda()
-del some_tensor
-print("here")
 
 torch.cuda.empty_cache()
 
-model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+model_id = "meta-llama/Llama-2-7b-chat-hf"
+model_id_gguf = "TheBloke/Llama-2-7B-Chat-GGUF"
+model_file_gguf = "llama-2-7b-chat.Q4_K_M.gguf"
+
 tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
-pipe = transformers.AutoModelForCausalLM.from_pretrained(
-    model_id,
-    low_cpu_mem_usage=True,
-    device_map='auto',
+pipe = ctransformers.AutoModelForCausalLM.from_pretrained(
+    model_id_gguf,
+    model_file=model_file_gguf,
+    model_type="llama",
+    gpu_layers=0,
 )
-#pipe = transformers.pipeline(
-#    "text-generation",
-#    model=model_id,
-#    gpu_layers=0,
-#    device_map='auto',
-#    load_in_8bit_fp32_cpu_offload=True,
-#    model_kwargs={"torch_dtype": torch.bfloat16, "load_in_4bit": True}
-#)
 
 messages = [
     {
@@ -29,11 +23,6 @@ messages = [
         "content": "You are an all knowing wizard who speaks in riddles",
     },
 ]
-
-#terminators = [
-#    pipe.tokenizer.eos_token_id,
-#    pipe.tokenizer.convert_tokens_to_ids("<|eot_id|>"),
-#]
 
 while True:
     user_input = input("\nUser: \n")
@@ -44,14 +33,9 @@ while True:
         "role": "user",
         "content": user_input,
     })
+    inputs = tokenizer.apply_chat_template(messages, tokenize=False)
     outputs = pipe(
-        tokenizer.encode(messages),
-#        max_new_tokens=16000,
-#        eos_token_id=terminators,
-#        do_sample=True,
-#        temperature=0.6,
-#        top_p=0.9,
+        inputs,
     )
-    assistant_response = outputs[0]["generated_text"][-1]["content"]
-    messages.append({"role": "assistant", "content": assistant_response})
-    print("\nAssistant: \n" + assistant_response)
+    messages.append({"role": "assistant", "content": outputs})
+    print("\nAssistant: \n" + outputs)
